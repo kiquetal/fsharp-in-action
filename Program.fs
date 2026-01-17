e892622e2bf1ff2d96deb1e53269da1b6aa98652
kiquetal <kiquetal@gmail.com>
2026-01-17 11:37:47 -0300

    refactor main function for improved readability

--- Program.fs
+++ Program.fs
@@ -3,6 +3,9 @@ open fsharp_in_action
 [<EntryPoint>]
 let main argv =
     match argv with
-    | [| "2" |] -> ChapterTwo.run()
-    | _ -> printfn "Please specify a chapter number (e.g., dotnet run -- 2)"
-    0
+    | [| "2" |] ->
+        ChapterTwo.run()
+        0
+    | _ ->
+        printfn "Please specify a chapter number (e.g., dotnet run -- 2)"
+        0
