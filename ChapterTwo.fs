b597c2313615a861a99dfd21c87b1ad635d7b7f7
kiquetal <kiquetal@gmail.com>
2026-01-17 11:31:57 -0300

    adding new instruction

--- ChapterTwo.fs
+++ ChapterTwo.fs
@@ -3,3 +3,7 @@ module fsharp_in_action.ChapterTwo
 
 let greet name =
     sprintf "Hello, %s!" name
+
+let run () =
+    let greeting = greet "F# Developer"
+    printfn "%s" greeting
--- Program.fs
+++ Program.fs
@@ -1,1 +1,8 @@
-﻿
+open fsharp_in_action
+
+[<EntryPoint>]
+let main argv =
+    match argv with
+    | [| "2" |] -> ChapterTwo.run()
+    | _ -> printfn "Please specify a chapter number (e.g., dotnet run -- 2)"
+    0
